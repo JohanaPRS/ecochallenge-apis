5f9152bc9525800a4d8991f2c0567b12c1e0ddf8
johana.rios95 <johana.rios@estudiantes.utec.edu.uy>
2021-11-20 22:49:00 -0300

    Update Swagger3Config.java

--- src/main/java/com/backend/jwt/api/config/Swagger3Config.java
+++ src/main/java/com/backend/jwt/api/config/Swagger3Config.java
@@ -11,13 +11,11 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
-@EnableSwagger2
 public class Swagger3Config {
     @Bean
     public Docket produceApi() {
